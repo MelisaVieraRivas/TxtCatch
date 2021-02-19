670790b16fc4fea3d7f8be6757834654a4c9639c
Portatil Mely <melisaviera@hotmail.com>
2021-02-19 16:47:23 +0100

    .

--- app/src/main/java/com/windows/txtcatch/MainActivity.java
+++ app/src/main/java/com/windows/txtcatch/MainActivity.java
@@ -1,14 +1,74 @@
 package com.windows.txtcatch;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageButton imageButton;
+    private final int RCODE = 28;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                capturarVoz();
+            }
+        });
     }
+
+    private void capturarVoz()
+    {
+        Intent intent = new Intent(RecognizerIntent
+                .ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        );
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(intent, RCODE);
+        } else
+        {
+            Log.e("ERROR","Su dispositivo no admite entrada de voz");
+        }
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode,
+                                     int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RCODE && resultCode == RESULT_OK && data != null)
+        {
+            ArrayList<String> result = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            textView.setText(result.get(0));
+        }
+
+    }
+
+
 }
